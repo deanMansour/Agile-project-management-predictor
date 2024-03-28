855b303459c236c2bcf502a6e0e4543cb4862a00
yaelalfa <yaelalfa0@gmail.com>
2024-03-28 19:30:55 +0200

    Adding Dashboard and navigation
    adding dashboard , navigation

---
+++ Predicting_app/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+# to extend from django built-in User creation form class, and link it to django built-in User model class
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+#=======================================================================================================
+""" To create class form that link to model.py,
+so with User_SignUp_Form_object.save() will execute User_new_object.save()
+class CustomUserCreationForm(forms.ModelForm): (to link to a class from model.py)   
+"""
+#   UserCreationForm is built in ModelForm for built-in User model class
+class User_SignUp_Form(UserCreationForm):
+    #no need for extra form fileds(User model class fields is enough)
+    class Meta:
+        # to link this class to built-In User class in models 
+        model = User
+        # field from User class that chosen to render it to template as form
+        # (password2 -->for validation)
+        fields = ('username', 'password1', 'password2')
+
+#===============================EXCEL TYPE FILES(xls/csv)================================================
+# class UploadExcelForm(forms.Form):
+#     excel_file = forms.FileField(label='Select a xls file')
+
+# class UploadCSVForm(forms.Form):
+#     csv_file = forms.FileField(label='Select a CSV file')
